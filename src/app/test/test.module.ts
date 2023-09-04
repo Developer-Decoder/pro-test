b80fe4462f66c78d3cf5a16e741ba3f820484032
Developer-Decoder <developerdecoder2@gmail.com>
2023-09-04 12:57:31 +0330

    first

---
+++ src/app/test/test.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestRoutingModule } from './test-routing.module';
+import { TestComponent } from './test.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+    declarations: [
+        TestComponent,
+        HomeComponent
+    ],
+    exports: [
+        TestComponent
+    ],
+    imports: [
+        CommonModule,
+        TestRoutingModule
+    ]
+})
+export class TestModule { }
